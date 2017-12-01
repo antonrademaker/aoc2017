ec1ee433a29b4926528c535fd54f69f194af4f52
Anton Rademaker <antonr@infosupport.com>
2017-12-01 15:47:16 +0100

    Day 1 - Part 2 solved

--- day1/Program.cs
+++ day1/Program.cs
@@ -6,28 +6,46 @@ namespace day1
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length < 2)
             {
                 Console.WriteLine("Test run");
 
+                Console.WriteLine("Part 1:");
 
+                Console.WriteLine(TestPart1("1122", 3));
+                Console.WriteLine(TestPart1("1111", 4));
+                Console.WriteLine(TestPart1("1234", 0));
+                Console.WriteLine(TestPart1("91212129", 9));
 
-                Console.WriteLine(Test("1122",3));
-                Console.WriteLine(Test("1111", 4));
-                Console.WriteLine(Test("1234", 0));
-                Console.WriteLine(Test("91212129", 9));
+                Console.WriteLine("Part 2:");
+
+                Console.WriteLine(TestPart2("1212", 6));
+                Console.WriteLine(TestPart2("1221", 0));
+                Console.WriteLine(TestPart2("123425", 4));
+                Console.WriteLine(TestPart2("123123", 12));
+                Console.WriteLine(TestPart2("12131415", 4));
 
                 return;
             }
 
-            Console.WriteLine(Calculate(args[0]));
-            
+            switch (args[0])
+            {
+                case "1":
+                    Console.WriteLine(CalculatePart1(args[1]));
+                    break;
+                case "2":
+                    Console.WriteLine(CalculatePart2(args[1]));
+                    break;
+            }
+
+
+
         }
 
-        static string Test(string input, long expected)
+        static string TestPart1(string input, long expected)
         {
 
-            var result = Calculate(input);
+            var result = CalculatePart1(input);
 
 
 
@@ -35,12 +53,12 @@ namespace day1
             return $"{(result == expected ? "Success" : "Fail")} {input}: {result} (expected: {expected})";
         }
 
-        static long Calculate(string s)
+        static long CalculatePart1(string s)
         {
-            return Calculate(s, 0, -1);
+            return CalculatePart1(s, 0, -1);
         }
 
-        static long Calculate(string s, int pos, int last)
+        static long CalculatePart1(string s, int pos, int last)
         {
             if (pos > s.Length)
             {
@@ -50,14 +68,48 @@ namespace day1
             {
                 if (current == last)
                 {
-                    return current + Calculate(s, pos + 1, current);
+                    return current + CalculatePart1(s, pos + 1, current);
                 }
 
-                return Calculate(s, pos + 1, current);
+                return CalculatePart1(s, pos + 1, current);
             }
             return 0;
         }
 
+
+        static string TestPart2(string input, long expected)
+        {
+
+            var result = CalculatePart2(input);
+
+
+
+
+            return $"{(result == expected ? "Success" : "Fail")} {input}: {result} (expected: {expected})";
+        }
+
+        static long CalculatePart2(string s)
+        {
+            return CalculatePart2(s, 0);
+        }
+
+        static long CalculatePart2(string s, int pos)
+        {
+            if (pos >= s.Length)
+            {
+                return 0;
+            }
+            if (int.TryParse(s[pos % s.Length].ToString(), out var current) && int.TryParse(s[(pos + (s.Length / 2)) % s.Length].ToString(), out var halfWay))
+            {
+                if (current == halfWay)
+                {
+                    return current + CalculatePart2(s, pos + 1);
+                }
+
+                return CalculatePart2(s, pos + 1);
+            }
+            return 0;
+        }
 
     }
 }
